4374ed141efbcae325eeba5ca743a1abc35c7fa7
Volodymyr Krasnoshapka <shlapa@gmail.com>
2021-05-28 15:24:51 +0300

    Added spinner component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Stocks from "./components/Stocks";
+import {useAppSelector} from "./redux/store";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const loading = useAppSelector((state) => state.base.loading);
+  
   return (
     <div className="App">
       <header>Portfolio analyzer</header>
+      {loading && <Spinner />}
       <Stocks />
     </div>
   );
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>Loading data from api...</div>
+  );
+}
+
+export default Spinner;
